f1a38a160b4832a161402ab40d99f9bcf16739f8
Josemacias354 <maciasbichillo1@gmail.com>
2024-01-09 12:11:58 +0100

    mi segundo commit

--- Prueba control versiones/src/tutgit/PrimeraClase.java
+++ Prueba control versiones/src/tutgit/PrimeraClase.java
@@ -4,7 +4,9 @@ public class PrimeraClase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//aqui el codigo
+		String nombre = ("Jose");
+		System.out.println("Hola");
 	}
 
 }
